30efef80d45ccd98f84de68535427bdf5d4f2ba4
Bogdan-Fai <faizullinbigdanzix@gmail.com>
2020-03-12 20:38:51 +0300

    qw

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,15 @@ class ArticlesController < ApplicationController
   def new
   end
 
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+
   def create
   @article = Article.new(params[:article])
 
